7eefc8ff1e5e0ab16892dd927b644ca279768690
ridhoartha <83918943+ridhoartha@users.noreply.github.com>
2021-06-28 20:07:02 +0700

    Update admins.py

--- MusicMan/modules/admins.py
+++ MusicMan/modules/admins.py
@@ -77,7 +77,7 @@ async def resume(_, message: Message):
 async def stop(_, message: Message):
     chat_id = get_chat_id(message.chat)
     if chat_id not in callsmusic.pytgcalls.active_calls:
-        await message.reply_text("❗ **Lagu nya gabisa di End,soalnya muka lu jelek sih**")
+        await message.reply_text("⚠️ **Lagu nya gabisa di end,soalnya muka lu jelek sih** ⚠️")
     else:
         try:
             callsmusic.queues.clear(chat_id)
@@ -85,7 +85,7 @@ async def stop(_, message: Message):
             pass
 
         callsmusic.pytgcalls.leave_group_call(chat_id)
-        await message.reply_text("❌ **Lagu nya gua berhentiin ya**")
+        await message.reply_text("✅ **Lagu Berhasil Berhenti**")
 
 
 @Client.on_message(command("skip") & other_filters)
@@ -95,7 +95,7 @@ async def skip(_, message: Message):
     global que
     chat_id = get_chat_id(message.chat)
     if chat_id not in callsmusic.pytgcalls.active_calls:
-        await message.reply_text("❗ **Lagu nya gabisa di skip soalnya lu sagapung sih**")
+        await message.reply_text("⚠️ **Bot nya ngambek soal nya dipake sama orang jelek sih** ⚠️")
     else:
         callsmusic.queues.task_done(chat_id)
 
@@ -106,7 +106,7 @@ async def skip(_, message: Message):
                 chat_id, callsmusic.queues.get(chat_id)["file"]
             )
 
-        await message.reply_text("⏩ **Melewati lagu**")
+        await message.reply_text("✅ **Lagu Berhasil Di Skip**")
 
 
 @Client.on_message(filters.command("admincache"))
