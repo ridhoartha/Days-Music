03434bc5b9d0c21119b43da82dee1da0125314f6
ridhoartha <83918943+ridhoartha@users.noreply.github.com>
2021-06-28 19:18:17 +0700

    Update msg.py

--- MusicMan/modules/msg.py
+++ MusicMan/modules/msg.py
@@ -29,38 +29,34 @@ f"""
 """,
 
 f"""
-**Pengaturan**
+**Cara Jalanin Bot nya**
 
 1. Jadikan bot sebagai admin
 2. Mulai obrolan suara / VCG
 3. Ketik `/userbotjoin` dan coba /play <nama lagu>
 × Jika Assistant Bot bergabung selamat menikmati musik, 
-× Jika Assistant Bot tidak bergabung Silahkan Tambahkan @{ASSISTANT_NAME} ke grup Anda dan coba lagi
+× Jika Assistant Bot tidak bergabung Silahkan Tambahkan Secara Manual @{ASSISTANT_NAME} ke grup Anda dan coba lagi
 
 
 **» Perintah Untuk dalam grup Member Juga Bisa :**
 
- × /playlist : Untuk Menampilkan daftar putar Lagu sekarang
- × /current : Untuk Menunjukkan  Lagu sekarang yang sedang diputar
- × /song <judul lagu> : Untuk Mendownload lagu di YouTube 
- × /video <judul lagu> : Untuk Mendownload Video di YouTube dengan detail
- × /vsong <judul lagu> : Untuk Mendownload Video di YouTube dengan detail
- × /deezer <judul lagu> : Untuk Mendownload lagu dari deezer 
- × /saavn <judul lagu> : Untuk Mendownload lagu dari website saavn
- × /search <judul lagu> : Untuk Mencari Video di YouTube dengan detail
-
-**» Perintah Hanya Untuk Admin :**
-
+× /playlist : Untuk Menampilkan daftar putar Lagu sekarang
+× /current : Untuk Menunjukkan  Lagu sekarang yang sedang diputar
+× /song <judul lagu> : Untuk Mendownload lagu di YouTube 
+× /video <judul lagu> : Untuk Mendownload Video di YouTube
+× /song <judul lagu> : Untuk Mendownload Video di YouTube 
+× /search <judul lagu> : Untuk Mencari Video di YouTube 
 × /play <judul lagu> : Untuk Memutar lagu yang Anda minta melalui youtube
 × /play <link yt> : Untuk Memutar lagu yang Anda minta melalui link youtube
 × /play <reply ke audio> : Untuk Memutar lagu yang Anda minta melalui file audio
-× /dplay : Untuk Memutar lagu yang Anda minta melalui deezer
-× /splay : Untuk Memutar lagu yang Anda minta melalui jio saavn
+
+**» Perintah Hanya Untuk Admin :**
+
 × /skip : Untuk Menskip pemutaran lagu ke Lagu berikutnya
 × /pause : Untuk Menjeda pemutaran Lagu
 × /resume : Untuk Melanjutkan pemutaran Lagu yang di pause
 × /end : Untuk Memberhentikan pemutaran Lagu
 × /userbotjoin - Untuk Mengundang asisten ke obrolan Anda
-× /admincache - Untuk MemRefresh admin list
+× /reload - Untuk MemRefresh admin list
 """
       ]
